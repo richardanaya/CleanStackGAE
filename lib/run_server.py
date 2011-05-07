5f380d2fe5e3bb4e4311268dbb86161c54965ad9
Richard Anaya <richard.anaya@gmail.com>
2011-05-07 16:15:25 -0500

    adding rest server

--- lib/run_server.py
+++ lib/run_server.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     import sys
     import subprocess
     import os
-    import wsgiref.handlers
+    #import wsgiref.hddandlers
+    from google.appengine.ext import webapp
+    from google.appengine.ext.webapp.util import run_wsgi_app
+    import rest
     sys.path.insert(0, 'lib.zip')
     import cherrypy
     import website
@@ -26,5 +29,11 @@ if __name__ == '__main__':
                 'tools.staticdir.on': True,
                 'tools.staticdir.dir': os.path.join(current_dir,"js") },
         }
-    app = cherrypy.tree.mount(website.Root(), "/",config)
-    wsgiref.handlers.CGIHandler().run(app)
+
+    rest_app = webapp.WSGIApplication([('/rest/.*', rest.Dispatcher)])
+    rest.Dispatcher.base_url = "/rest"
+    cherrypy.tree.graft(rest_app,'/rest')
+    cherrypy.tree.mount(website.Root(), "/",config)
+    run_wsgi_app(cherrypy.tree)
+    
+ 
