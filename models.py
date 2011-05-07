ded03b9451f16d8094ca9a6ce000aa9cd7a636ca
Richard Anaya <richard.anaya@gmail.com>
2011-05-07 16:22:24 -0500

    updating models file to be more useful, adding simple README

--- models.py
+++ models.py
@@ -1,1 +1,27 @@
+from google.appengine.ext import db
+import rest
 #Domain models go here
+
+#EXAMPLES:
+#class Restaurant(db.Model):
+#    name = db.StringProperty()
+#    image_background = db.ReferenceProperty(RestaurantImage)
+#    num_employees = db.IntegerProperty()
+#    is_open = db.BooleanProperty();
+
+#Expose domain models here
+
+#EXAMPLES:
+# add all models from the current module, and/or...
+#   rest.Dispatcher.add_models_from_module(__name__)
+# add all models from some other module, and/or...
+#   rest.Dispatcher.add_models_from_module(my_model_module)
+# add specific models
+#   rest.Dispatcher.add_models({
+#        "foo": FooModel,
+#        "bar": BarModel})
+# add specific models (with given names) and restrict the supported methods
+#   rest.Dispatcher.add_models({
+#      'foo' : (FooModel, rest.READ_ONLY_MODEL_METHODS),
+#      'bar' : (BarModel, ['GET_METADATA', 'GET', 'POST', 'PUT'],
+#      'cache' : (CacheModel, ['GET', 'DELETE'] })
