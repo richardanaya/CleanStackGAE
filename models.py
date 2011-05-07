c2e28c5557eb2417122ea7e8311fda4acecf4050
Richard Anaya <richard.anaya@gmail.com>
2011-05-07 16:25:47 -0500

    cleaning

--- models.py
+++ models.py
@@ -9,19 +9,22 @@ import rest
 #    num_employees = db.IntegerProperty()
 #    is_open = db.BooleanProperty();
 
-#Expose domain models here
+#Expose domain models as REST here
 
 #EXAMPLES:
-# add all models from the current module, and/or...
-#   rest.Dispatcher.add_models_from_module(__name__)
-# add all models from some other module, and/or...
-#   rest.Dispatcher.add_models_from_module(my_model_module)
+
+#Add all models from the current module, and/or...
+#rest.Dispatcher.add_models_from_module(__name__)
+
+#Add all models from some other module, and/or...
+#rest.Dispatcher.add_models_from_module(my_model_module)
 # add specific models
-#   rest.Dispatcher.add_models({
+#rest.Dispatcher.add_models({
 #        "foo": FooModel,
 #        "bar": BarModel})
-# add specific models (with given names) and restrict the supported methods
-#   rest.Dispatcher.add_models({
+
+#Add specific models (with given names) and restrict the supported methods
+#rest.Dispatcher.add_models({
 #      'foo' : (FooModel, rest.READ_ONLY_MODEL_METHODS),
 #      'bar' : (BarModel, ['GET_METADATA', 'GET', 'POST', 'PUT'],
 #      'cache' : (CacheModel, ['GET', 'DELETE'] })
